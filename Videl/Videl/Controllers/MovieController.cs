3e2e9f53144b296b7d7217fd7a7796cae804eb18
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-03 15:17:41 -0600

    Add Movie class

---
+++ Videl/Videl/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videl.Models;
+using Videl.ViewModels;
+
+namespace Videl.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Sharek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+    }
+}
