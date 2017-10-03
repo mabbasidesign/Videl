c23d8bc709c38d9f3625562d2f767a4d81a06abb
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-03 11:03:08 -0600

    Initial

---
+++ Videl/Videl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Videl.Startup))]
+namespace Videl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
