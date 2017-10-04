9bdc21fae15d411e7bc3a0327c4f1958a00ec117
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-04 05:28:42 -0600

    Adding Customers

--- Videl/Videl/Controllers/MovieController.cs
+++ Videl/Videl/Controllers/MovieController.cs
@@ -30,14 +30,32 @@ namespace Videl.Controllers
             return View(viewModel);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(string.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        //    return Content(string.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        //}
+
+        public ActionResult Index()
+        {
+            var movie = GetMovies();
+
+            return View(movie);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie { Id = 1, Name ="Sherek" },
+                new Movie { Id = 2, Name ="Hak" }
+            };
+        }
+
+        
     }
 }
---
+++ Videl/Videl/Controllers/customerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videl.Models;
+
+namespace Videl.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: customer
+        public ActionResult Index()
+        {
+            var customer = GetCustomer();
+
+            return View(customer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomer()
+                .SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
