a1ffa76682d37aa459510947b1dea51685a1b955
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-05 12:13:03 -0600

    Extend Move & Add Details

--- Videl/Videl/Controllers/MovieController.cs
+++ Videl/Videl/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Videl.Models;
 using Videl.ViewModels;
 
@@ -10,6 +11,18 @@ namespace Videl.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
@@ -42,7 +55,18 @@ namespace Videl.Controllers
 
         public ActionResult Index()
         {
-            var movie = GetMovies();
+            var movie = _context.Movies
+                .Include(c => c.Genre)
+                .ToList();
+
+            return View(movie);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies
+                .Include(c => c.Genre)
+                .SingleOrDefault(c => c.Id == id);
 
             return View(movie);
         }
